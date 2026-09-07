3acf086183fa60d4060b55b2a6441db1c4e72665
Penyoy <penyoy11@gmail.com>
2026-09-07 11:32:22 +0000

    fix: jvm target 17 toolchain for ksp vs javac

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
+    }
+    kotlin {
+        jvmToolchain(17)
     }
     buildFeatures {
         compose = true

6a16cfa259a05e1779a1c0318d006708106911c3
Penyoy <penyoy11@gmail.com>
2026-09-07 18:11:15 +0700

    Update minSdk version from 21 to 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.anivers.anime"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"

2b4881404a18f1de751bfa5b863ce2820edc0ea9
Penyoy <penyoy11@gmail.com>
2026-09-07 18:19:50 +0700

    Add Material Components dependency
    Added Material Components library for theming.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
+// Material (untuk Theme.MaterialComponents parent di themes.xml)
+    implementation("com.google.android.material:material:1.12.0")
+
+
     // Splash
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.13.1")

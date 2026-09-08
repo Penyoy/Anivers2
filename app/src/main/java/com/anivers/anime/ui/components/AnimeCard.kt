f0b9ab8fcd54f89a4592915beb396eae41837bf7
Penyoy <penyoy11@gmail.com>
2026-09-08 02:33:27 +0000

    fix: missing rememberInteractionSource + tween imports

--- app/src/main/java/com/anivers/anime/ui/components/AnimeCard.kt
+++ app/src/main/java/com/anivers/anime/ui/components/AnimeCard.kt
@@ -3,6 +3,7 @@ package com.anivers.anime.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.interaction.rememberInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
